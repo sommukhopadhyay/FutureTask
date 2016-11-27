0820ae140fd0869a7434611d1bb6802f3e4e4bb1
som <som.mukhopadhyay@gmail.com>
2016-11-27 16:56:35 +0530

    Some Improvements like use of Threadpool, etc.

--- src/com/somitsolutions/training/java/ExperimentationWithFutureTask/Main.java
+++ src/com/somitsolutions/training/java/ExperimentationWithFutureTask/Main.java
@@ -1,39 +1,58 @@
 package com.somitsolutions.training.java.ExperimentationWithFutureTask;
 
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args){
+		List<ProductInfo> listOfProductInfo = null;
+		System.out.println(Thread.currentThread().getName());
+		
 		Preloader preloader = new Preloader();
 		
+		//start getting the data in a background thread and
+		//keep it for future use. while the background
+		//thread is getting the data, we will continue
+		//other task. later we can get this already fetched data
+		//using future.get method. remember this get API is blocking
 		preloader.start();
-		while (!preloader.isDone()){
-			System.out.println("Task is not completed yet...."); 
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} //sleep for 1 millisecond before checking again
-		}
 		
+		/*//Do some other works... Here we are making the main thread sleep
 		try {
-			Thread.sleep(10000);
+			Thread.sleep(50000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		ProductInfo pInfo = preloader.get();
-		
-		//This commented block shows how we can cancel a Future task
-		/*if (!preloader.cancel() == true){
-			System.out.println("The task has been cancelled...");
-		}
-		else{
-			System.out.println(pInfo.getProductName());
-			System.out.print(pInfo.getProductPrice());	
 		}*/
 		
-		System.out.println(pInfo.getProductName());
-		System.out.print(pInfo.getProductPrice());	
+		int count = 0;
+		while (!preloader.isDone()){
+			System.out.println("Task is yet to be completed...");
+			count++;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				Preloader.executor.shutdown();
+				System.exit(0);
+			} //sleep for 1 millisecond before checking again
+			
+			if (count == 100){//make the count == a low number say 8
+				//to see the cancellation effect
+				preloader.cancel();
+				System.out.println("The task has been cancelled...");
+				Preloader.executor.shutdown();
+				System.exit(0);
+				}
+			}
+		
+			if(!preloader.cancel() && preloader.isDone()){
+				listOfProductInfo = preloader.get();
+			}
+		
+		System.out.println(listOfProductInfo.get(0).getProductName());
+		System.out.print(listOfProductInfo.get(0).getProductPrice());
+		
+		Preloader.executor.shutdown();
 	}
 }
--- src/com/somitsolutions/training/java/ExperimentationWithFutureTask/Preloader.java
+++ src/com/somitsolutions/training/java/ExperimentationWithFutureTask/Preloader.java
@@ -1,23 +1,30 @@
 package com.somitsolutions.training.java.ExperimentationWithFutureTask;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.Callable;
 
 public class Preloader {
 	
-		private final FutureTask<ProductInfo> future = new FutureTask<ProductInfo>(new Callable<ProductInfo>(){
-				public ProductInfo call() {
-					return loadProductInfo();
-					}
-				}){
-		};
+		static ExecutorService executor  = Executors.newFixedThreadPool(1);
+		List<ProductInfo> productInfo = new ArrayList<ProductInfo>();
+		//The difference between Callable & Runnable 
+		//is that Callable can return a value (of type futuretask)
+		private FutureTask<List<ProductInfo>> future = null;
 		
-		public ProductInfo get(){
-			ProductInfo retValue = null;
+		/*new FutureTask<List<ProductInfo>>(new LoadProductInfo());*/
+				
+		
+		public List<ProductInfo> get(){
+			//List<ProductInfo> retValue = null;
 			try {
-				retValue =  future.get();
+				//get is blocking
+				productInfo =  future.get();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -25,7 +32,7 @@ public class Preloader {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			return retValue;
+			return productInfo;
 		}
 		
 		public boolean cancel(){
@@ -36,20 +43,43 @@ public class Preloader {
 			return future.isDone();
 		}
 		
-		private final Thread thread = new Thread(future);
+		//private final Thread thread = new Thread(future);
 		
-		public void start() { thread.start(); }
+		public void start() {
+			System.out.println("The task is being submitted now...");
+			//submit will return immediately. So we can do the other work
+			//in the main thread. Later we can check if the task is
+			//finished or not using isDone method.
+			future = (FutureTask<List<ProductInfo>>) (executor.submit(new LoadProductInfo())); 
+			}
 		
-		private ProductInfo loadProductInfo(){
+		//long running task
+		private List<ProductInfo> loadProductInfo(){
+			System.out.println(Thread.currentThread().getName());
 			try {
-				Thread.sleep(5000);
+				Thread.sleep(10000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			return new ProductInfo("ABC", 100);
+			//As if this data we have got from 
+			//the database
+			for (int i = 0; i<100000; i++){
+				ProductInfo productI = new ProductInfo(Integer.toString(i), i);
+				productInfo.add(productI);
+			}
+			return productInfo;
 		}
-		
-		
+		//The difference between Callable & Runnable 
+		//is that Callable can return a value (of type futuretask)	
+		class LoadProductInfo implements Callable<List<ProductInfo>>{
+	
+			@Override
+			public List<ProductInfo> call() throws Exception {
+				// TODO Auto-generated method stub
+				return loadProductInfo();
+			}
+			
+		}
 }
 
